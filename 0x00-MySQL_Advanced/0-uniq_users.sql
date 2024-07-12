fd6655c8f98f822c96c5969b46673217e834148f
tAf <muzirat0@gmail.com>
2024-07-12 17:13:01 +0000

    moved not null constraint to id

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,7 @@
 -- creates users table in database
--- creates database if it does not exist
-CREATE DATABASE IF NOT EXISTS holberton;
-CREATE TABLE IF NOT EXISTS holberton.users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255) NOT NULL
+	name VARCHAR(255)
 );
 
