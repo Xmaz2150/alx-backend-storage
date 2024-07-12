c306ed99d62baa068d3c9f278f4a40642abb3952
tAf <muzirat0@gmail.com>
2024-07-12 17:19:02 +0000

    moved not null constraint to id

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,8 @@
 -- creates users table in database
--- creates database if it does not exist
-CREATE DATABASE IF NOT EXISTS holberton;
-CREATE TABLE holberton.users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255) NOT NULL,
+	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
 
