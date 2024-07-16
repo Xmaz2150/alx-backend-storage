b93e74f9449649abd116658383d054feeafc9713
tAf <muzirat0@gmail.com>
2024-07-16 11:05:30 -0700

    refactor

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,21 +2,27 @@
 """
 provides stats about Nginx logs stored in MongoDB
 """
-
-
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-
+def server_stats(mongo_collection):
+    """
+    computes stats from collection
+    """
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    print(nginx_logs.count_documents({}), " logs")
-    print("methods:")
+    print(mongo_collection.count_documents({}), "logs")
+    print("Methods:")
 
     for method in methods:
-        m_count = nginx_logs.count_documents({"method": method})
+        m_count = mongo_collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, m_count))
-    status = nginx_logs.count_documents({"method": "GET", "path": "/status"})
+    status = mongo_collection.count_documents({"method": "GET", "path": "/status"})
     print("{} status check".format(status))
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    nginx_logs = db.nginx
+    
+    server_stats(nginx_logs)
